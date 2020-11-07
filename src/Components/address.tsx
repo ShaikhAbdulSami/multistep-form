8f2079b8b1d866c2e917b4b85e35cd55215421cd
Shaikh Abdul Sami <65606480+ShaikhAbdulSami@users.noreply.github.com>
2020-11-07 14:55:45 +0500

    Initial

---
+++ src/Components/address.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
+import { Button, TextField } from "@material-ui/core";
+
+interface Props {
+  handleNext: () => void;
+}
+
+const Address: React.FC<Props> = ({ handleNext }) => {
+  return (
+    <Formik
+      initialValues={{ country: "", state: "", city: "" , address: "" }}
+      validationSchema={Yup.object({
+        country: Yup.string().required("Required").min(4, "Invalid"),
+        state: Yup.string().required("Required").min(4, "Invalid"),
+        city: Yup.string().required("Required").min(4, "Invalid"),
+        address: Yup.string().required("Required").max(200 , "Invalid")
+      })}
+      onSubmit={(values) => {
+        setTimeout(() => {
+          console.log(JSON.stringify(values, null, 2));
+          handleNext();
+        }, 400);
+      }}
+    >
+      {({ errors, touched, dirty, isValid }) => (
+        <Form>
+          <Field
+            as={TextField}
+            label="Country"
+            variant="standard"
+            name="country"
+            type="text"
+            error={touched.country && errors.country}
+            helperText={touched.country && errors.country}
+          />
+          <br />
+          <br />
+          <Field
+            as={TextField}
+            label="State"
+            variant="standard"
+            name="state"
+            type="text"
+            error={touched.state && errors.state}
+            helperText={touched.state && errors.state}
+          />
+          <br />
+          <br />
+          <Field
+            as={TextField}
+            label="City"
+            variant="standard"
+            name="city"
+            type="text"
+            error={touched.city && errors.city}
+            helperText={touched.city && errors.city}
+          />
+          <br />
+          <br />
+          <Field
+            as={TextField}
+            label="Address"
+            variant="standard"
+            name="address"
+            type="text"
+            error={touched.address && errors.address}
+            helperText={touched.address && errors.address}
+          />
+          <br />
+          <br />
+          <Button
+            variant="contained"
+            color="primary"
+            disabled={!isValid || !dirty}
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default Address;
---
+++ src/Components/personal.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
+import TextField from "@material-ui/core/TextField";
+import {Button} from "@material-ui/core";
+
+interface Props {
+  handleNext: () => void;
+}
+
+const Personal: React.FC<Props> = ({ handleNext }) => {
+  return (
+    <Formik
+      initialValues={{ firstName: "", lastName: "", age: 0 }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        lastName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+      })}
+      onSubmit={(values) => {
+        setTimeout(() => {
+          console.log(JSON.stringify(values, null, 2));
+
+          handleNext();
+        }, 400);
+      }}
+    >
+      {({ errors, touched, dirty, isValid }) => (
+        <Form>
+          <Field
+            as={TextField}
+            variant="standard"
+            label="First Name"
+            name="firstName"
+            type="text"
+            error={errors.firstName && touched.firstName}
+            helperText={touched.firstName && errors.firstName}
+          />
+          <br />
+          <br />
+          <Field
+            as={TextField}
+            variant="standard"
+            label="Last Name"
+            name="lastName"
+            type="text"
+            error={errors.lastName && touched.lastName}
+            helperText={touched.lastName && errors.lastName}
+          />
+          <br />
+          <br />
+          <Field
+            as={TextField}
+            variant="standard"
+            label="Birthday"
+            type="date"
+            InputLabelProps={{
+                shrink: true,
+            }}
+            helperText="optional"
+          />
+          <br />
+          <br />
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            disabled={!dirty || !isValid}
+          >
+            Next
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default Personal;
